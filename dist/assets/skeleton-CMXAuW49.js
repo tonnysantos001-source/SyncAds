ed96c04e09256baa05b24fc6cb9d106418b8f8a0
nobregasilvio4-max <tonnysantos001@gmail.com>
2025-11-12 16:36:00 -0300

    revert: Restaurar versao estavel anterior - commit da3aee75

---
+++ dist/assets/skeleton-CMXAuW49.js
@@ -0,0 +1,1 @@
+import{j as s}from"./vendor-ui-D3JNL6pE.js";import{h as e}from"./index-BPTW8-dN.js";function a({className:a,...m}){return s.jsx("div",{className:e("animate-pulse rounded-md bg-muted",a),...m})}export{a as S};
